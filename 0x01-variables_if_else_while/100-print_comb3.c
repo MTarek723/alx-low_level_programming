b7da39c5b064a8fc16eb097b85a9f79e6f358d3c
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-11 15:56:37 -0700

    L

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+int y;
+x = 0;
+while (x <= 9)
+{
+y = 0;
+while (y <= 9)
+{
+if (y > x && x != y)
+{
+putchar(x + 48);
+putchar(y + 48);
+if (y +x != 17)
+{
+putchar(44);
+putchar(32);
+}
+}
+++y;
+}
+++x;
+}
+putchar('\n');
+return (0);
+}
