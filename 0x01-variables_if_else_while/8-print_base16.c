e93f6ed87a551663a64e345120d0046509de5196
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-11 12:11:54 -0700

    L

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+int y;
+for (x = 48; x < 58; ++x)
+{
+putchar(x);
+}
+for (y = 65; y < 71; ++y)
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
