ed7a1e0e3443aab86bd41a310c1636878a0de118
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-15 13:33:55 -0700

    l

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <"main.h">
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+int a;
+for (a = 97; a <= 122; ++x)
+{
+putchar(a);
+putchar('\n');
+return (0);
+}
