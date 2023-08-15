1da5dabb724eaca49bdb738170240cb9a32dd62f
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-15 14:22:52 -0700

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* print_alphabet - function to print alphabet in lower case
-*/ 
+*print_alphabet - function to print alphabet in lower case
+*/
 void print_alphabet(void)
 {
 int a;
