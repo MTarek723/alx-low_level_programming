71e7aa8ec02a5d0b0dd06c8b004288fecdeb65f7
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-17 15:48:54 -0700

    L

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,16 @@
 void print_line(int n)
 {
 	int c;
-	if ( n <= 0 )
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	for (c = 0; c <= n; c++)
+	else
 	{
-		_putchar('_');
+		for (c = 0; c <= n; c++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
