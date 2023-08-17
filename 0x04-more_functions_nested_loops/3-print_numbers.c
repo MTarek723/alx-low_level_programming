82cfce8a1eb55124b852ddef97698ada78e86481
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-17 15:21:10 -0700

    L

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+void print_numbers(void)
+{
+	int x;
+	for(x = 48; x < 58; ++x)
+	{
+
+		_putchar(x);
+	}
+	_putchar('\n');
+}
