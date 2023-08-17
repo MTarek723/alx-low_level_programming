9cf1a043fcc570dc718fb5f733d3d02253fc4e97
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-17 16:21:48 -0700

    L

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,22 @@
 #include "main.h"
+
 /**
  * more_numbers - print more numbers
  */
+
 void more_numbers(void)
 {
-	int x;
-	int c;
-	for (c = 1; c <= 10; c++)
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
 	{
-		for (x = 0; x <= 14; x++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (x >= 10)
-			{
+			if (j >= 10)
 				_putchar('1');
-			}
-			_putchar(x % 10 + '0');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
 }
+
