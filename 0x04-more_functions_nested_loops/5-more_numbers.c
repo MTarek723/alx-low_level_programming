06ca05becafa5bedc873e441ce501555f7c47078
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-17 15:29:36 -0700

    L

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers
+ */
+void more_numbers(void)
+{
+	int x;
+	int c;
+	for (c = 0; c <= 9; c++)
+	{
+		for (x = 0; x < 15; ++x)
+		{
+			if (x < 10)
+			{
+				_putchar(x);
+			}
+			else if (x > 10)
+			{
+				_putchar(x / 10 + '0');
+				_putchar(x % 10 + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
