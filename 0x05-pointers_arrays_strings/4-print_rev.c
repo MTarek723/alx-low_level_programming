6ae3ef4de23812dd654097282d1dc36c8d3ee355
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-21 15:04:28 -0700

    L

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int y = 0;
+	int x;
+
+	while (*s != '\0')
+	{
+		y++;
+		s++;
+	}
+	s--;
+	for (x = y; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
+
