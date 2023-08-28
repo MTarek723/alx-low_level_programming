6081c2c70d12131643348fb82169897d3b0fff80
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2023-08-28 09:47:15 -0700

    L

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int x;
-	
-	x = 0;
+	int r = 0;
+	int i = n;
 
-	while (x < n)
+	for (; r < i; r++)
 	{
-		dest[x] = src[x];
-		++x;
+		dest[r] = src[r];
+		n--;
 	}
 	return (dest);
 }
